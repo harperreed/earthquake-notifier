b1e28631fe194db78e5f357bfeddd83e52d1a765
Harper Reed <harper@nata2.org>
2024-01-01 21:09:33 -0700

    Implement alert tracking with Firestore and add new app icon                                                                   (base)
                                                                             - Added checkIfAlertSent and markAlertAsSent functions to prevent duplicate earthquake alerts using Firestore.
                                                                             - Integrated Firebase Admin SDK for server-side Firestore access.
                                                                             - Included a new app icon representing seismic activity and alerts.

---
+++ functions/ai.js
@@ -0,0 +1,46 @@
+/* eslint-disable require-jsdoc */
+
+const { OpenAI } = require("openai");
+
+const logger = require("firebase-functions/logger");
+
+
+async function getAISummary(data) {
+  const dataStr = JSON.stringify(data);
+  // eslint-disable-next-line max-len
+  const earthQuakeBotPrompt = `
+  You are a news reporter that is doing a live blog about an earthquake(s).
+  You are given a list of earthquakes in JSON format. 
+  Your job is to explain the data in English.
+  Don't mention the data directly. 
+  Use bullets where necessary.
+  You summarize the data in english.
+  Limit to 3 major points. Be concise. Don't be verbose.
+  You can use html to style it (b, i, and u tags)`;
+
+  const model = process.env.OPENAI_QUICKMODEL;
+
+  const openAiCompletionArgs = {
+    model: model,
+    messages: [
+      { "role": "system", "content": earthQuakeBotPrompt },
+      { "role": "user", "content": dataStr },
+    ],
+  };
+
+  const openai = new OpenAI();
+  // openai.baseURL = "https://api.dud.org/api/v1";
+  openai.baseURL = "https://openaiproxy-baxvbakvia-uc.a.run.app/v1";
+  const response = await openai.chat.completions.create(openAiCompletionArgs);
+  // logger.info({ response });
+
+  const aiResponse = response.choices[0].message.content;
+  console.log(aiResponse);
+  return aiResponse;
+}
+
+
+module.exports = {
+  getAISummary,
+};
+
---
+++ functions/index.js
@@ -0,0 +1,160 @@
+/* eslint-disable require-jsdoc */
+const { onRequest } = require("firebase-functions/v2/https");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const { onSchedule } = require("firebase-functions/v2/scheduler");
+
+const axios = require("axios");
+const Pushover = require('pushover-notifications')
+const { getAISummary } = require("./ai");
+// Initialize Firebase
+initializeApp();
+
+const db = getFirestore();
+
+// Function to check for earthquakes with parameters
+async function checkEarthquake(latitude, longitude, radius) {
+  const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${radius}`;
+  console.log(url);
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    let alertPriority = -1;
+    const earthquakeData = [];
+    if (data.features && data.features.length > 0) {
+      for (const earthquake of data.features) {
+        const earthquakeInfo = earthquake.properties;
+        const earthquakeId = earthquake.id; // Unique ID for the earthquake
+
+        // Check if an alert for this earthquake ID has already been sent
+        const isAlertSent = await checkIfAlertSent(earthquakeId);
+        if (isAlertSent) {
+          continue; // Skip this earthquake as an alert has already been sent
+        }
+
+
+        // console.log(earthquakeInfo.title);
+        switch (true) {
+          case (earthquakeInfo.mag >= 5.0 && earthquakeInfo.mag < 6.0):
+            if (alertPriority < 0) {
+              alertPriority = 0;
+            }
+            break;
+          case (earthquakeInfo.mag >= 6.0 && earthquakeInfo.mag < 8.0):
+            if (alertPriority < 1) {
+              alertPriority = 1;
+            }
+
+            break;
+          case (earthquakeInfo.mag >= 8.0):
+            if (alertPriority < 2) {
+              alertPriority = 2;
+            }
+            break;
+        }
+        earthquakeData.push(earthquake);
+      }
+
+      if (earthquakeData.length == 0) {
+        return "No new earthquakes detected.";
+      }
+
+      console.log(earthquakeData.length);
+      const message = await manufactureAlert(JSON.stringify(earthquakeData));
+
+      await sendAlert(message, alertPriority);
+
+      // Mark all earthquakes as having sent an alert
+      for (const earthquake of earthquakeData) {
+        const earthquakeId = earthquake.id;
+        await markAlertAsSent(earthquakeId);
+      }
+
+      return message;
+    } else {
+      return "No significant earthquakes detected.";
+    }
+  } catch (error) {
+    console.error("Error checking for earthquakes:", error);
+    return "Error occurred while checking for earthquakes.";
+  }
+}
+
+
+// Function to send Pushover alert
+async function manufactureAlert(data) {
+  const summary = await getAISummary(data);
+  return summary;
+}
+
+// Function to send Pushover alert
+async function sendAlert(message, priority) {
+  const pushoverConfig = {
+    token: process.env.PUSHOVER_TOKEN, // Replace with your Pushover API token
+    user: process.env.PUSHOVER_USER, // Replace with your Pushover user key
+  };
+
+  // Initialize Pushover
+  const pushover = new Pushover(pushoverConfig);
+  const msg = {
+    message: message,
+    html: 1,
+    priority: priority,
+  };
+  if (priority > 1) {
+    msg.expire = 3600;
+    msg.retry = 180;
+  }
+  console.log(msg);
+  pushover.send(msg, (err, result) => {
+    if (err) {
+      console.error("Error sending alert:", err);
+    } else {
+      console.log("Alert sent:", result);
+    }
+  });
+}
+
+// Function to check if an alert has already been sent for a given earthquake ID
+async function checkIfAlertSent(earthquakeId) {
+  try {
+    const doc = await db.collection("sent_alerts").doc(earthquakeId).get();
+    return doc.exists;
+  } catch (error) {
+    console.error("Error checking alert status:", error);
+    throw error; // You can decide to handle this differently
+  }
+}
+
+// Function to mark an earthquake ID as having sent an alert
+async function markAlertAsSent(earthquakeId) {
+  try {
+    await db.collection("sent_alerts").doc(earthquakeId).set({ sent: true });
+    console.log(`Alert marked as sent for earthquake ID: ${earthquakeId}`);
+  } catch (error) {
+    console.error("Error marking alert as sent:", error);
+    throw error; // You can decide to handle this differently
+  }
+}
+
+// HTTP Triggered Function
+exports.earthquakeCheck = onRequest(async (req, res) => {
+  // Default values or use query parameters
+  const latitude = req.query.lat || "35.662139";
+  const longitude = req.query.lng || "138.568222";
+  const radius = req.query.radius || "100";
+
+  const result = await checkEarthquake(latitude, longitude, radius);
+  res.send(result);
+});
+
+exports.earthquakeCheckCrontab = onSchedule("0 */1 * * *", async (event) => {
+  // Default values or use query parameters
+  const latitude = "35.662139";
+  const longitude = "138.568222";
+  const radius = "100";
+
+  const result = await checkEarthquake(latitude, longitude, radius);
+  console.log(result);
+  return result;
+});
